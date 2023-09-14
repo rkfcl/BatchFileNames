5aa8c6e76a7e1ca6ff726a35dc7f0a79b24ce61d
rkfcl <kwmkr6@gmail.com>
2023-09-14 19:20:24 +0900

    파일명일괄 변경2.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,62 +1,64 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 사용자로부터 디렉토리 경로 입력받기
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("디렉토리 경로를 입력하세요: ");
-        String directoryPath = scanner.nextLine();
-
-        // 사용자로부터 변경하고자 하는 파일명 입력받기
-        System.out.print("변경하고자 하는 파일명을 입력하세요: ");
-        String baseFileName = scanner.nextLine();
-
-        // 사용자로부터 제외하고 싶은 확장자들 입력받기 (콤마(,)로 구분)
-        System.out.print("제외하고 싶은 확장자들을 입력하세요 (예: txt,pdf): ");
-        String excludedExtensionsInput = scanner.nextLine();
-        scanner.close(); // 스캐너 닫기
-
-        // 디렉토리 객체 생성
-        File directory = new File(directoryPath);
-
-        // 디렉토리가 존재하는지 확인
-        if (!directory.exists()) {
-            System.out.println("디렉토리가 존재하지 않습니다.");
-            return;
-        }
-
-        // 입력받은 확장자들을 콤마(,)로 분리
-        String[] excludedExtensions = excludedExtensionsInput.split(",");
-
-        // 디렉토리 안의 파일 목록 가져오기
-        File[] files = directory.listFiles();
-
-        // 파일 이름 변경
-        int count = 1;
-        for (File file : files) {
-            if (file.isFile()) { // 파일인 경우에만 처리
-                String fileName = file.getName();
-                String extension = getFileExtension(fileName);
-
-                // 제외하고 싶은 확장자가 아닌 경우에만 처리
-                if (!isExcludedExtension(extension, excludedExtensions)) {
-                    String newName = baseFileName + "_" + count + extension;
-                    File newFile = new File(directory, newName);
-                    if (file.renameTo(newFile)) {
-                        System.out.println("파일 이름 변경 성공: " + fileName + " -> " + newName);
-                    } else {
-                        System.out.println("파일 이름 변경 실패: " + fileName);
-                    }
-                    count++;
-                }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
             }
-        }
+        });
     }
 
-    // 파일의 확장자를 가져오는 메서드
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("File Renamer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setMultiSelectionEnabled(true);
+
+        int result = fileChooser.showOpenDialog(frame);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File[] selectedFiles = fileChooser.getSelectedFiles();
+
+            // 사용자 입력을 받을 커스텀 대화 상자 생성
+            CustomDialog dialog = new CustomDialog(frame);
+            dialog.setVisible(true);
+
+            String newFileName = dialog.getNewFileName();
+
+            if (newFileName != null && !newFileName.isEmpty()) {
+                // 선택한 파일들에 대해 이름 변경 로직을 수행
+                for (int i = 0; i < selectedFiles.length; i++) {
+                    File file = selectedFiles[i];
+                    String extension = getFileExtension(file.getName());
+                    String newName = newFileName + "_" + (i + 1) + extension;
+                    File newFile = new File(file.getParentFile(), newName);
+
+                    if (file.renameTo(newFile)) {
+                        System.out.println("파일 이름 변경 성공: " + file.getName() + " -> " + newName);
+                    } else {
+                        System.out.println("파일 이름 변경 실패: " + file.getName());
+                    }
+                }
+
+                JOptionPane.showMessageDialog(frame, "파일 이름 변경이 완료되었습니다.");
+                createAndShowGUI();
+            } else {
+                JOptionPane.showMessageDialog(frame, "새로운 파일명을 입력하세요.");
+            }
+        }
+
+        frame.dispose();
+    }
+
     private static String getFileExtension(String fileName) {
         int lastDotIndex = fileName.lastIndexOf(".");
         if (lastDotIndex != -1) {
@@ -64,15 +66,37 @@ public class Main {
         }
         return "";
     }
-
-    // 확장자가 제외할 확장자 목록에 포함되어 있는지 확인하는 메서드
-    private static boolean isExcludedExtension(String extension, String[] excludedExtensions) {
-        for (String excludedExtension : excludedExtensions) {
-            if (extension.equalsIgnoreCase("." + excludedExtension.trim())) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 
+class CustomDialog extends JDialog {
+    private JTextField textField;
+    private String newFileName;
+
+    public CustomDialog(JFrame parent) {
+        super(parent, "새 파일명 입력", true);
+        setLayout(new BorderLayout());
+
+        textField = new JTextField(20);
+        JButton okButton = new JButton("확인");
+
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                newFileName = textField.getText();
+                dispose();
+            }
+        });
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(new JLabel("새 파일명: "));
+        inputPanel.add(textField);
+        inputPanel.add(okButton);
+
+        add(inputPanel, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(parent);
+    }
+
+    public String getNewFileName() {
+        return newFileName;
+    }
+}
